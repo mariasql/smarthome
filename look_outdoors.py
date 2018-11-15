dca58ada32fafba8e4fc20956c5304804f0ea0e8
Maria Zakourdaev <Maria.Zakourdaev@vonage.com>
2018-11-15 17:11:52 +0200

    Remove ips

--- look_outdoors.py
+++ look_outdoors.py
@@ -8,7 +8,7 @@ from slacker import Slacker
 import json
 
 config_file = sys.argv[1]
-#config_file = 'window_camera.yaml'
+
 config_content = open(config_file)
 config = json.load(config_content)
 
@@ -17,9 +17,7 @@ os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;0"
 # Below code will capture the video frames and will sve it a folder (in current working directory)
 os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;0"
 
-#camera_ip = "192.168.1.108"
 camera_ip = config.get("camera_ip", "")
-#camera_url = "rtsp://admin:admin@{}/onvif1".format(camera_ip)
 camera_url = config.get("camera_url", "").format(camera_ip)
 
 slack_token = config.get("slack_token", "")
@@ -29,7 +27,6 @@ slack_channel = config.get("slack_channel", "")
 def capture_snapshot():
     dirname = r""
     # video path
-    # cap = cv2.VideoCapture("rtsp://admin:admin@192.168.1.102/onvif1", cv2.CAP_FFMPEG)
     cap = cv2.VideoCapture(camera_url, cv2.CAP_FFMPEG)
 
     if cap.isOpened():
