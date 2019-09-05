a323e7f1079fdaff52152aa753c5f9ea7fe814a6
mariasql <maria.zakourdaev@gmail.com>
2019-09-05 12:09:34 +0300

    remove socket.close()

--- internet_test.py
+++ internet_test.py
@@ -25,7 +25,6 @@ def post_slack(text_msg,slack_url):
 def is_connected():
     try:
         socket.create_connection(("www.google.com", 10)
-
         return True
     except OSError:
         pass
