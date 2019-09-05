27d3eaed4118780925005fab05882c23715f9d94
mariasql <maria.zakourdaev@gmail.com>
2019-09-05 09:53:43 +0300

    initial creation

---
+++ internet_test.py
@@ -0,0 +1,40 @@
+import socket
+import requests
+import time
+
+config_content = open("../config_files/config.yaml")
+config = json.load(config_content)
+slack_url = config.get("slack_url", "")
+
+def post_slack(text_msg,slack_url):
+    webhook_url = slack_url
+    slack_data = {'text': "{}".format(text_msg)}
+
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
+
+
+def is_connected():
+    try:
+        # connect to the host -- tells us if the host is actually
+        # reachable
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError:
+        pass
+    return False
+
+
+log_time = time.strftime("%Y%m%d-%H%M%S")
+with open('../internet_status.txt', 'a+') as f:
+    if is_connected():
+        f.write("{},{}".format(log_time,'true'))
+    else:
+        f.write("{},{}".format(log_time,'false'))
