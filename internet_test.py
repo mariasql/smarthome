6d76f396e78763aa0c21c47c720da0348ff7bfe0
mariasql <maria.zakourdaev@gmail.com>
2019-09-08 12:49:19 +0300

    add read_and_clean function

--- internet_test.py
+++ internet_test.py
@@ -2,10 +2,13 @@ import socket
 import requests
 import time
 import json
+import csv
 
-#config_content = open("../config_files/config.yaml")
-#config = json.load(config_content)
-#slack_url = config.get("slack_url", "")
+config_content = open("../config_files/config.yaml")
+config = json.load(config_content)
+slack_url = config.get("slack_url", "")
+
+
 
 def post_slack(text_msg,slack_url):
     webhook_url = slack_url
@@ -30,9 +33,24 @@ def is_connected():
         pass
     return False
 
+def read_and_clean():
+    with open('../internet_status.csv', 'rb') as f:
+        reader = csv.reader(f)
+        all_records = list(reader)
+        chtt = ''
+        chd = ''
+        for record in all_records:
+            chtt += '{},'.format(record[0])
+            chd += '{},'.format(record[1])
+
+        chartt =  '<img src="https://image-charts.com/chart?chs=700x125&chd=t:{}&chtt={}"</p>'.format(
+                    chd, chtt)
+
+        print (chartt)
+
 
 log_time = time.strftime("%Y%m%d-%H%M%S")
-with open('../internet_status.txt', 'a+') as f:
+with open('../internet_status.csv', 'a+') as f:
     if is_connected():
         f.write("{},{}\r\n".format(log_time,'true'))
         print ('Connected to the internet')
